d5b01b3eda9fc148a2f97f930e523e25e695b38c
bryanmejiapalma <brmejiapalma@s.sfusd.edu>
2026-02-04 12:59:24 -0800

    player

--- player.gd
+++ player.gd
@@ -1,31 +1,19 @@
 extends CharacterBody2D
 
-<<<<<<< Updated upstream
+
 const WALK_SPEED = 300
 const BOOST_SPEED = 600
 
 # Gravity from project settings
 
-=======
-var max_speed := boost_speed
-var boost_speed := 1000
-var SPEED := 150.0
-var current_speed := SPEED
-var sprint_multiplier = 1.6
->>>>>>> Stashed changes
 
 func _physics_process(delta: float) -> void:
 	# Apply gravity
-# Add the gravity. 
 
-<<<<<<< Updated upstream
-=======
-	# Handle jump.
->>>>>>> Stashed changes
 
 	var vertical_input := Input.get_axis("ui_down", "ui_up")
 
-<<<<<<< Updated upstream
+
 	# Horizontal input (left/right)
 	var horizontal_input := Input.get_axis("ui_left", "ui_right", )
 
@@ -39,9 +27,9 @@ func _physics_process(delta: float) -> void:
 	velocity.x = horizontal_input * current_speed
 	velocity.y = vertical_input * current_speed
 	# Move the character with collisions
-=======
+
 	if  Input.is_action_just_pressed("boost"):
-		current_speed *=  sprint_multiplier
+		current_speed *=  BOOST_SPEED
 	
->>>>>>> Stashed changes
+
 	move_and_slide()
