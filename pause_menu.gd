e69300f9362cc24648ac21a8485941985f11df97
Keantonio <Keantonio@s.sfusd.edu>
2026-04-29 13:47:41 -0700

    pause screen

---
+++ pause_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+var pause_toggle = false
+
+func _ready() -> void: 
+	self.visible = false
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		pause_and_unpause()
+
+func pause_and_unpause():
+	pause_toggle = !pause_toggle
+	get_tree().paused = pause_toggle
+	self.visible = pause_toggle
