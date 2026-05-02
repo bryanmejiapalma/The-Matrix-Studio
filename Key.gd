82218e2d54114e140d0675ec60c2e58f8defe1e7
bryanmejiapalma <brmejiapalma@s.sfusd.edu>
2026-05-01 19:43:46 -0700

    key pick up

--- Key.gd
+++ Key.gd
@@ -1,28 +1,9 @@
 extends Area2D
 
+@export var item_texture: Texture2D
 
-var is_held = false
-var player_in_range = null 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	body_entered.connect(_on_body_entered)
-	body_exited.connect(_on_body_exited)
-	collision_mask = 1 
+func _ready():
+	add_to_group("pickable")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("interact"):
-		if not is_held and player_in_range != null:
-			pick_up(player_in_range)
-		elif is_held:
-			drop_Key()
-			
-func pick_up(player_node: Node2D) -> void:
-	pass
-
-func _on_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
-
-
-func _on_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+func get_texture():
+	return item_texture
