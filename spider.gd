f191fce5f8f5cba244e78009aed31c081d4603fd
geneyvar <gelopezbarragan@s.sfusd.edu>
2026-05-18 15:50:53 -0700

    spider walking animation

--- spider.gd
+++ spider.gd
@@ -6,17 +6,22 @@ var damage = 8
 var player = null
 var spawn_amount = 5
 
+# Grab a reference to your AnimatedSprite2D node
+@onready var animated_sprite = $AnimatedSprite2D
+
 # Ensure these paths match your FileSystem EXACTLY!
-# If your files are "Apple.tscn" with a capital A, change these!
 var apple_scene = preload("res://apple.tscn")
 var banana_scene = preload("res://banana.tscn")
-
 
 func _ready():
 	# Explicitly add to group in code to be safe
 	add_to_group("enemy")
 	player = get_tree().get_first_node_in_group("player")
 	print("Spider spawned and ready!")
+	
+	# Start playing your walk animation immediately when spawning
+	if animated_sprite:
+		animated_sprite.play("walk")
 
 func _physics_process(_delta):
 	# Added a check to make sure player still exists
@@ -29,6 +34,10 @@ func _physics_process(_delta):
 			velocity = dir * speed
 			rotation = dir.angle() 
 			move_and_slide()
+			
+			# Keep playing the walk animation while moving
+			if animated_sprite and velocity.length() > 0:
+				animated_sprite.play("walk")
 
 # Make sure your Area2D signal is connected to this function!
 func _on_attack_area_body_entered(body):
@@ -45,22 +54,21 @@ func take_damage(amount):
 	if health <= 0:
 		print("Spider died!")
 		spawn_loot()
-		queue_free() 
+		queue_free() # This removes the spider from the game when it dies
 
 func spawn_loot():
 	var roll = randf() 
-	var loot = null
+	var loot = null 
 	
 	if roll < 0.15: 
-		loot = apple_scene.instantiate()
+		loot = apple_scene.instantiate() 
 		print("Dropped an apple!")
 	elif roll < 0.30: 
-		loot = banana_scene.instantiate()
+		loot = banana_scene.instantiate() 
 		print("Dropped a banana!")
 	else:
 		print("No loot dropped this time.")
 	
-	if loot:
-		# We add to current_scene so the loot doesn't disappear when the spider does
+	if loot: 
 		get_tree().current_scene.call_deferred("add_child", loot)
 		loot.global_position = global_position
